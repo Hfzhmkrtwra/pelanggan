2ae9e82954ba2b2e712955b330ea192947234366
Hfzhmkrtwra <hafizahkartawiria701@gmail.com>
2025-01-13 10:06:48 +0700

    menambah konfigurasi firebase punya saya sendiri

--- main.js
+++ main.js
@@ -11,3 +11,14 @@ import {
   query,
   orderBy
 } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGYnq4VKq-YGu4RbfoI_ZHez9fishYjZo",
+  authDomain: "insan-cemerlang-afd2f.firebaseapp.com",
+  projectId: "insan-cemerlang-afd2f",
+  storageBucket: "insan-cemerlang-afd2f.appspot.com",
+  messagingSenderId: "686649580589",
+  appId: "1:686649580589:web:61374bbbd68adb604eaca4",
+  measurementId: "G-LNZTQBCE26"
+};
